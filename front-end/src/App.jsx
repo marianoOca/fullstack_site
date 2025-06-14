a8c07ffecb14a0cbd138b16cd2453a8471205795
El Marian <marianoagoca@gmail.com>
2025-06-14 16:41:29 -0300

    added Log In page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,6 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
-import axios from 'axios';
 import './App.css'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage';
@@ -10,6 +9,8 @@ import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage,  { loader as articleLoader } from './pages/ArticlePage'
 import Layout from './Layout';
 import NotFoundPage from './pages/notFound';
+import LoginPage from './pages/LogingPage';
+import CreateAccountPage from './pages/CreateAccount';
 
 const routes = [{
   path: '/',
@@ -28,6 +29,12 @@ const routes = [{
     path: '/articles/:name',
     element: <ArticlePage />,
     loader: articleLoader
+  }, {
+    path: '/login',
+    element: <LoginPage />
+  }, {
+    path: '/create-account',
+    element: <CreateAccountPage />
   }]
 }]
 
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlPoxQ6oQHTwNzHhAHdsui9ftWjT5m-xg",
+  authDomain: "fullstack-site-86ab2.firebaseapp.com",
+  projectId: "fullstack-site-86ab2",
+  storageBucket: "fullstack-site-86ab2.firebasestorage.app",
+  messagingSenderId: "653460471066",
+  appId: "1:653460471066:web:253ce8ffdcdc28c32180a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />

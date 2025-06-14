d43d72b46f8df7c58680c3b69590f5e5911f3d87
El Marian <marianoagoca@gmail.com>
2025-06-14 01:04:24 -0300

    connected to mongo db server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,17 +1,40 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const articleInfo = [
-    { name: 'eternauta', upvotes: 0, comments: []},
-    { name: 'amigos', upvotes: 0, comments: []},
-    { name: 'socialismo', upvotes: 0, comments: []},
+    { name: 'learn-node', upvotes: 0, comments: [] },
+    { name: 'learn-react', upvotes: 0, comments: [] },
+    { name: 'mongodb', upvotes: 0, comments: [] },
 ]
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+        }
+    });
+
+    await client.connect();
+
+    const db = client.db('full-stack-react-db');
+
+    const article = await db.collection('articles').findOne({ name });
+
+    res.json(article);
+});
+
 app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = articleInfo.find(a =>a.name === req.params.name);
+    const article = articleInfo.find(a => a.name === req.params.name);
     article.upvotes += 1;
 
     res.json(article);

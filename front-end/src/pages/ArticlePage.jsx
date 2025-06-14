3ac92f3954ab1e66d562287623235810255e5f61
El Marian <marianoagoca@gmail.com>
2025-06-14 15:45:22 -0300

    added full functional comments

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,28 +1,40 @@
+import { useState } from 'react';
 import { useParams, useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CommentsList from '../CommentsList';
+import AddCommentForm from '../AddCommentForm';
 import articles from '../article-content';
-import { useState } from 'react';
 
-export default function ArticlePage(){
+export default function ArticlePage() {
     const { name } = useParams();
-    const { upvotes: initialUpvotes, comments } = useLoaderData();
+    const { upvotes: initialUpvotes, comments: initialComments } = useLoaderData();
     const [upvotes, setUpvotes] = useState(initialUpvotes);
+    const [comments, setComments] = useState(initialComments);
 
     const article = articles.find(a => a.name === name);
 
-    async function onUpvotedClicked() {
+    async function onUpvoteClicked() {
         const response = await axios.post('/api/articles/' + name + '/upvote');
         const updatedArticleData = response.data;
         setUpvotes(updatedArticleData.upvotes);
     }
 
+    async function onAddComment({ nameText, commentText }) {
+        const response = await axios.post('/api/articles/' + name + '/comments', {
+            postedBy: nameText,
+            text: commentText,
+        });
+        const updatedArticleData = response.data;
+        setComments(updatedArticleData.comments);
+    }
+
     return (
         <>
         <h1>{article.title}</h1>
-        <button onClick={onUpvotedClicked}>Upvote</button>
+        <button onClick={onUpvoteClicked}>Upvote</button>
         <p>This article has {upvotes} upvotes</p>
         {article.content.map(p => <p key={p}>{p}</p>)}
+        <AddCommentForm onAddComment={onAddComment} />
         <CommentsList comments={comments} />
         </>
     );
